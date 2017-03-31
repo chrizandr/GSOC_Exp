bdb84529762fdd1479901b737ec6ae4646aca9f9
Chris Andrew <chris.g14@iiits.in>
2017-03-31 22:43:55 +0530

    Initial implementation

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+linked_data = json.load(open("Spacecraft.json", "r"))
+entrypoint = "/"
+
+# Base url, serves api documentation and the entrypoint
+@app.route('/')
+def entrypoint():
+    global linked_data
+    print(request.path)
+    return (str(linked_data["@context"]))
